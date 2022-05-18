78bd1146362f943e2994763c849dd93a1302d815
Prince Kumar <princekumar0320@gmail.com>
2022-05-18 09:23:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isEditing, setIsEditing] = useState(0);
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  function handleDelete(id) {
+    const delTodo = todos.filter((element) => element.id !== id);
+
+    setTodos([...delTodo]);
+    // console.log(typeof delTodo);
+  }
+
+  function handleEdit(id) {
+    const editedTodo = todos.find((element) => element.id === id);
+    setInput(editedTodo.input);
+    setIsEditing(id);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(isEditing);
+    if (isEditing) {
+      const editedTodo = todos.find((ele) => ele.id === isEditing);
+      // console.log(editedTodo);
+      const updatedTodo = todos.map((ele) =>
+        ele.id === editedTodo.id
+          ? (ele = { id: ele.id, input })
+          : { id: ele.id, input: ele.input }
+      );
+      console.log(updatedTodo);
+      setTodos(updatedTodo);
+      setInput("");
+      setIsEditing(0);
+      return;
+    }
+
+    if (input !== "") {
+      setTodos((prevState) => [
+        { id: `${input}-${Date.now()}`, input },
+        ...prevState,
+      ]);
+      setInput("");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>ToDo List</h1>
+        <form className="todoform" onSubmit={handleSubmit}>
+          <input type="text" value={input} onChange={handleChange} />
+          <button type="submit">{isEditing ? "Edit" : "Add"}</button>
+        </form>
+        <ul className="allTodo">
+          {todos.map((list) => (
+            <li className="singleTodo">
+              <span className="text" id={list.id} key={list.id}>
+                {list.input}
+              </span>
+              <button onClick={() => handleEdit(list.id)}>Edit</button>
+              <button onClick={() => handleDelete(list.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
